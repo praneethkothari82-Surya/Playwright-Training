a2f3da4c6d37d96fc76a8ddb4081eec36b0e1deb
praneethkothari <praneethk.automation@gmail.com>
2025-11-14 14:02:53 +0530

    Increase test timeout to 60 seconds

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,6 +19,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  /* Maximum time one test can run for */
+  timeout: 60000,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
